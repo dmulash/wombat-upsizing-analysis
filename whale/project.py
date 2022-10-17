aa62ec0b09ae46c637e5a3a04ee70f1fd0e580a1
Rob Hammond <rob.hammond@nrel.gov>
2022-10-17 15:17:11 -0600

    create theoretically working model

--- whale/project.py
+++ whale/project.py
@@ -1,16 +1,15 @@
 from lib2to3.pytree import convert
 from pathlib import Path
-from typing import Type
 
 import attrs
+import pandas as pd
 from attrs import define, field
-from numpy import isin
 
-from orbit import ProjectManager
+from ORBIT import ProjectManager
 from wombat.core import Simulation
+from floris.tools import FlorisInterface
 from wombat.core.library import load_yaml
 from wombat.core.data_classes import FromDictMixin
-from floris.tools import FlorisInterface
 
 
 def resolve_path(value: str | Path) -> Path:
@@ -34,27 +33,42 @@ def resolve_path(value: str | Path) -> Path:
 
 
 def read_config(value: str | Path | dict | ProjectManager | Simulation | FlorisInterface) -> dict | ProjectManager | Simulation | FlorisInterface:
-    if isinstance(value, (ProjectManager, Simulation, FlorisInterface, dict)):
-        return value
     if isinstance(value, (str, Path)):
-        return resolve_path(value)
+        value = load_yaml(resolve_path(value))
+    if isinstance(value, dict):
+        return value
     raise TypeError(
         (
             f"The input configuration is not a valid format: {type(value)}. Inputs must"
-            " be of type `str`, `Path`, `dict`, or simulation specific: "
-            "`orbit.ProjectManager`, `wombat.core.Simulation`, or "
-            "`floris.tools.FlorisInterface`."
+            " be of type `str`, `Path`, or `dict`."
         )
     )
 
 
 @define
 class Project(FromDictMixin):
+    """The unified interface for creating, running, and assessing analyses that combine
+    ORBIT, WOMBAT, and FLORIS.
+
+    Args:
+        library_path(:obj:`str` | :obj:`pathlib.Path`): The file path where the configuration data
+            for ORBIT, WOMBAT, and FLORIS can be found.
+        weather(:obj:`str` | :obj:`pathlib.Path`): The file path where the weather
+            profile data is located.
+        orbit_config(:obj:`str` | :obj:`pathlib.Path`): The ORBIT configuration file name or dictionary.
+        wombat_config(:obj:`str` | :obj:`pathlib.Path`): The WOMBAT configuration file name or dictionary.
+        floris_config(:obj:`str` | :obj:`pathlib.Path`): The FLORIS configuration file name or dictionary.
+    """
     library_path: str | Path = field(converter=resolve_path)
     weather: str | Path = field(converter=resolve_path)
-    orbit_config: Path | dict | ProjectManager = field(converter=read_config)
-    wombat_config: Path | dict | Simulation = field(converter=read_config)
-    floris_config: Path | dict | FlorisInterface = field(converter=read_config)
+    orbit_config: str | Path | dict = field(converter=read_config)
+    wombat_config: str | Path | dict = field(converter=read_config)
+    floris_config: str | Path | dict = field(converter=read_config)
+
+    # Internally created attributes, aka, no user inputs to these
+    wombat: Simulation = field(init=False)
+    orbit: ProjectManager = field(init=False)
+    floris: FlorisInterface = field(init=False)
 
     def __attrs_post_init__(self) -> None:
         self.setup_orbit()
@@ -63,17 +77,34 @@ class Project(FromDictMixin):
 
     @library_path.validator
     def library_exists(self, attribute: attrs.Attribute, value: str | Path) -> None:
+        """Validates that the user input to :py:attr:`library_path` is a valid directory.
+
+        Args:
+            attribute (attrs.Attribute): The attrs Attribute information/metadata/configuration.
+            value (str | Path): The user input.
+
+        Raises:
+            FileNotFoundError: Raised if :py:attr:`value` does not exist.
+            ValueError: Raised if the :py:attr:`value` exists, but is not a directory.
+        """
         if not value.exists():
             raise FileNotFoundError(f"The input path to {attribute.name} cannot be found: {value}")
         if not value.is_dir():
             raise ValueError(f"The input path to {attribute.name}: {value} is not a directory.")
 
-    def setup_orbit(self):
-        ...
+    def setup_orbit(self) -> None:
+        """Creates the ORBIT Project Manager object and readies it for running an analysis."""
+        self.orbit = ProjectManager(self.orbit_config, library_path=self.library_path, weather=pd.read_csv(self.weather))
 
-    def setup_wombat(self):
-        ...
+    def setup_wombat(self) -> None:
+        """Creates the WOMBAT Simulation object and readies it for running an analysis."""
+        self.wombat = Simulation(library_path=self.library_path, config=self.wombat_config)
 
-    def setup_floris(self):
-        ...
+    def setup_floris(self) -> None:
+        """Creates the FLORIS FlorisInterface object and readies it for running an analysis."""
+        self.floris = FlorisInterface(configuration=self.floris_config)
 
+    def run(self) -> None:
+        self.orbit.run()
+        self.wombat.run()
+        self.floris.calculate_wake()
